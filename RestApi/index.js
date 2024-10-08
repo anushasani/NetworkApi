29095e038ceb563a8424bf291a25040c83672e3a
anushasani <anusha.sani55@gmail.com>
2024-10-07 17:11:59 -0700

    Network

--- RestApi/index.js
+++ RestApi/index.js
@@ -1,29 +1,73 @@
-// import express from "express";
-
-// const app = express();
-
-// app.all("/", (req, res) => {
-//   console.log("req", req);
-//   console.log("res", res);
-//   res.send("I am up!");
-// });
-
-// const PORT = "5111";
-// app.listen(PORT, () => {
-//   console.log(`Server is running at port ${PORT}`);
-// });
-
 import express from "express";
+import bodyParser from "body-parser";
+
 const app = express();
 
 app.all("/", (req, res) => {
   console.log("req", req);
   console.log("res", res);
-  res.send("i am app");
+  res.send("I am up!");
 });
 
-const PORT = "12345";
+app.use(bodyParser.json());
+const toDo = [
+  {
+    id: "1",
+    task: "Task1",
+    completed: false,
+  },
+  {
+    id: "2",
+    task: "Task2",
+    completed: true,
+  },
+];
 
+//Read
+app.get("/toDo", (req, res) => {
+  res.json(toDo);
+});
+
+//Create
+app.post("/toDo", (req, res) => {
+  const newToDo = req.body;
+  toDo.push(newToDo);
+  res.json({
+    message: "new  Todo is Added",
+  });
+});
+//Update
+app.put("/toDo/:id", (req, res) => {
+  const newToDobody = req.body;
+
+  const toDoIndex = toDo.findIndex((td) => td.id === req.params.id);
+
+  if (toDoIndex !== -1) {
+    toDo[toDoIndex] = {
+      id: req.params.id,
+      ...newToDobody,
+    };
+  }
+
+  res.json({
+    message: "ToDO Updated Successfully",
+  });
+});
+
+app.delete("/toDo/:id", (req, res) => {
+  const toDoParamId = req.params.id;
+  const toDoIndex = toDo.findIndex((td) => td.id === toDoParamId);
+
+  if (toDoIndex !== -1) {
+    toDo.splice(toDoIndex, 1);
+  }
+
+  res.json({
+    message: "ToDO Deleted Successfully",
+  });
+});
+
+const PORT = "5111";
 app.listen(PORT, () => {
-  console.log(`Server is running at Port ${PORT}`);
+  console.log(`Server is running at port ${PORT}`);
 });
